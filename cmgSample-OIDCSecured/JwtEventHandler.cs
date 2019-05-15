b26f0385f2c0b88000361e962d2094b2f6d90c36
unknown <andreas@masseyfam.com>
2019-05-15 18:30:04 -0400

    Initial Commit

---
+++ cmgSample-OIDCSecured/JwtEventHandler.cs
@@ -0,0 +1,86 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+// Added these imports
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Diagnostics;
+using System.Security.Claims;
+
+
+namespace cmgSample_OIDCSecured.Security
+{
+    /// <summary>
+    /// A JWT validation event handler.
+    /// </summary>
+    public static class JwtEventHandler
+    {
+        /// <summary>
+        /// Creates one or more JWT event handlers.
+        /// </summary>
+        /// <returns>
+        /// A JwtBearerEvents object instance.
+        /// </returns>
+        public static JwtBearerEvents CreateJwtEvents()
+        {
+            var events = new JwtBearerEvents
+            {
+                // This event happens when the JWT token is present, but is not valid
+                OnAuthenticationFailed = context =>
+                {
+                    Debug.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+
+                    return Task.FromResult(0);
+                },
+                // This event happens when the JWT token is valid.
+                // the logic inspects the JWT token for claims, and converts them
+                // to standard Microsoft claims so the built-in .NET functionality can be used
+                // e.g. [Authorize(Roles = "Everyone")] decoration
+                OnTokenValidated = context =>
+                {
+                    Debug.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                    // re-adding as claims which .net core understands for some standard role-based authorization
+                    var claims = new List<Claim>();
+
+                    // evaluate the "username" claim in the token, and convert to a Microsoft Role claim with a value of "Everyone"
+                    //populating custom claim - can be used to embed custom security info
+                    int claimUserIDVal = context.Principal.Claims.Where(c => c.Type == "username" && c.Value.Length > 0).Count();
+
+                    if (claimUserIDVal > 0)
+                    {
+                        claims.Add(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", "Everyone"));
+                    }
+
+                    // iterate through the "MemberOf" claim in the token, and convert to .NET claims (one claim for each role group)
+                    // Example value: "memberof": "CN=R-App-CSM-AASSecondLevelSupport:CN=R-SERVICEGATEWAY-MyInvestmentView-NonProd:CN=R-SERVICEGATEWAY-MyInvestmentView-P:CN=R-App-Pkg-Microsoft_VisualStudio_2017:CN=R-App-Pkg-AltovaXMLSpy2018"
+                    int claimMemberOfVal = context.Principal.Claims.Where(c => c.Type == "memberof" && c.Value.Length > 0).Count();
+                    if (claimMemberOfVal > 0)
+                    {
+                        string[] membersofList = context.Principal.Claims.Where(c => c.Type == "memberof").FirstOrDefault().Value.ToString().Split(":");
+                        foreach (string strmemberof in membersofList)
+                        {
+                            claims.Add(new Claim("http://schemas.microsoft.com/ws/2008/06/identity/claims/role", strmemberof.Split(",")[0].Replace("CN=", "")));
+                        }
+                    }
+
+                    if (claims.Count > 0)
+                    {
+                        // claims were found in the token and converted to Microsoft claims,
+                        // so add them to the context Principal
+                        var appIdentity = new ClaimsIdentity(claims);
+                        context.Principal.AddIdentity(appIdentity);
+                    }
+
+                    return Task.FromResult(0);
+                }
+            };
+
+            return events;
+        }
+    }
+}
---
+++ cmgSample-OIDCSecured/Startup.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+// added this import to support OIDC support
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using cmgSample_OIDCSecured.Security;
+using Microsoft.IdentityModel.Logging;
+
+namespace cmgSample_OIDCSecured
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // Replace your ConfigureServices method with the following:
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+            // Change compatibility mode to 2_2
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // This is the .Net core JWT Authentication Service
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               //This OIDC Discovery URL should be pulled from the environment parameters.
+               //Each consuming app should get their own client_id and subscribe to the OIDC Discovery API in the approriate catalog. 
+               options.MetadataAddress = Configuration["OIDCDiscoveryURL"];
+
+               // Hook into the custom JWT event handler class which performs custom claims mapping and error handling.
+               options.Events = JwtEventHandler.CreateJwtEvents();
+               options.Audience = "CUNA:JWT";               
+           }
+
+           );
+
+            //this is an example of how to connect an Authorization policy. 
+            //"SiteAdmin" policy which requires a specific claim value to map true for authorization to pass.
+            //RoleGroup can be pulled from the environment configuration.
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("IsInRoleForLookUpsPolicy", policy =>
+                                policy.RequireRole(Configuration["LookUpRoleGroup"]) );
+            });
+
+            // This property can be used to troubleshoot JWT validation errors. It adds more details for validation errors.
+            //It can be commented out for production deploy.
+            //It also requires Microsoft.IdentityModel.Logging;
+
+            IdentityModelEventSource.ShowPII = true;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            // This line below is required to wire up JWT authentication.
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
+
+            app.UseMvc();
+        }
+
+        
+    }
+}
